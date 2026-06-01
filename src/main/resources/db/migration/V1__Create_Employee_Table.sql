32d642b9d2cb908e27c506c5fbe63f1491d38e39
Keerthi Bandari <keerthibandari123@gmail.com>
2026-06-01 22:23:14 +0530

    Added Task 2 Spring Boot output screenshot

---
+++ src/main/resources/db/migration/V1__Create_Employee_Table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE employee (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100),
+    department VARCHAR(100)
+);
---
+++ src/main/resources/db/migration/V2__Insert_Sample_Data.sql
@@ -0,0 +1,5 @@
+INSERT INTO employee (name, email, department)
+VALUES ('Keerthi', 'keerthi@gmail.com', 'CSE');
+
+INSERT INTO employee (name, email, department)
+VALUES ('Rahul', 'rahul@gmail.com', 'IT');
